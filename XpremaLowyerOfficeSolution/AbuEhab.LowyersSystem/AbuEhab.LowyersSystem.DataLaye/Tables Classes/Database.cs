fb05a102659190ea3eeb4df399f0bca3a4040996
Abu Ehab <MyGlassBox@Hotmail.Com>
2016-01-27 15:21:55 +0200

    Start Create Classes

---
+++ XpremaLowyerOfficeSolution/AbuEhab.LowyersSystem/AbuEhab.LowyersSystem.DataLaye/Tables Classes/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbuEhab.LowyersSystem.DataLaye.Tables_Classes
+{
+    public   abstract  class Database
+    {
+
+        public static DbDataContext context = new DbDataContext();
+    }
+}
---
+++ XpremaLowyerOfficeSolution/AbuEhab.LowyersSystem/AbuEhab.LowyersSystem.DataLaye/Tables Classes/EmployeeCmd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbuEhab.LowyersSystem.DataLaye.Tables_Classes
+{
+    public class EmployeeCmd : Database 
+    {
+
+
+
+
+    }
+}
