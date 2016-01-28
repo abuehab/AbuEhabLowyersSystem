b1824ef5ba61c9ed91f3d7e0484a0fad7cdce8f4
omda-shareef <omda515@hotmail.co.uk>
2016-01-28 11:24:39 +0300

    NewLawyer Method

---
+++ XpremaLowyerOfficeSolution/AbuEhab.LowyersSystem/AbuEhab.LowyersSystem.DataLaye/Tables Classes/LowyerCmd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbuEhab.LowyersSystem.DataLaye.Tables_Classes
+{
+    public class LowyerCmd : Database 
+    {
+
+
+        // Go Do It 
+        public bool NewLowyer(Lawyer lo)
+        {
+            context =new DbDataContext();
+            context.Lawyers.InsertOnSubmit(lo);
+            context.SubmitChanges();
+            return true;
+        }
+    }
+}
